51b058f6b2fb0c0f689c11c5c6f37ce7c3e83a37
Ariel Richtman <10679234+arichtman@users.noreply.github.com>
2024-08-17 15:24:33 +1000

    boo, not working still

--- flake.nix
+++ flake.nix
@@ -1,70 +1,70 @@
 {
-  description = "A Nix-flake-based Rust development environment";
+  description = "blg";
 
-  inputs = {
-    nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-    rust-overlay = {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+  inputs.rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-  };
 
-  outputs = { self, nixpkgs, rust-overlay }:
-    let
-      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f: nixpkgs.lib.genAttrs supportedSystems (system: f {
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [ rust-overlay.overlays.default self.overlays.default ];
-        };
-      });
-    in
-    {
-      overlays.default = final: prev: {
-        rustToolchain =
-          let
-            rust = prev.rust-bin;
-          in
-          if builtins.pathExists ./rust-toolchain.toml then
-            rust.fromRustupToolchainFile ./rust-toolchain.toml
-          else if builtins.pathExists ./rust-toolchain then
-            rust.fromRustupToolchainFile ./rust-toolchain
-          else
-            rust.stable.latest.default.override {
-              extensions = [ "rust-src" "rustfmt" ];
-            };
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay
+  }: let
+    # Should work with other targets, but not tested.
+    supportedSystems = ["x86_64-linux"];
 
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-            micronucleus
-            # Need avr-gcc binary
-            # gcc
-            # avrdude
-            # arduino-core
-            # arduino
-            # simavr
-            pkgsCross.avr.buildPackages.gcc
-            # pkgsCross.avr.buildPackages.avrlibc
-          ];
-          # Just for quicker iteration
-          shellHook = "cargo build";
+    # Helper function to generate an attrset '{ x86_64-linux = f "x86_64-linux"; ... }'.
+    forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
 
-          env = {
-            # Required by rust-analyzer
-            RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
-            # RUSTC_BOOTSTRAP=1 is necessary because this target has not been fully stabilized yet and is still subject to change, you may need to make changes to stay compatible with future Rust compilers.
-            RUSTC_BOOTSTRAP= 1;
+    # Nixpkgs instantiated for supported system types.
+    nixpkgsFor = forAllSystems (system: import nixpkgs {inherit system;
+    overlays = [ rust-overlay.overlays.default self.overlays.default ];
+    });
+  in {
+    packages = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system}.pkgsStatic;
+    in {
+    });
+
+    overlays.default = final: prev: {
+      rustToolchain =
+        let
+          rust = prev.rust-bin;
+        in
+        if builtins.pathExists ./rust-toolchain.toml then
+          rust.fromRustupToolchainFile ./rust-toolchain.toml
+        else if builtins.pathExists ./rust-toolchain then
+          rust.fromRustupToolchainFile ./rust-toolchain
+        else
+          rust.stable.latest.default.override {
+            extensions = [ "rust-src" "rustfmt" ];
           };
-        };
-      });
     };
+    devShells = forAllSystems (system: let
+      pkgs = nixpkgsFor.${system};
+    in {
+      default = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          rustToolchain
+          openssl
+          pkg-config
+          cargo-deny
+          cargo-edit
+          cargo-watch
+          rust-analyzer
+          micronucleus
+          pkgsCross.avr.buildPackages.gcc
+        ];
+        env = {
+          # Required by rust-analyzer
+          RUST_SRC_PATH = "${pkgs.rustToolchain}/lib/rustlib/src/rust/library";
+          # RUSTC_BOOTSTRAP=1 is necessary because this target has not been fully stabilized yet and is still subject to change, you may need to make changes to stay compatible with future Rust compilers.
+          RUSTC_BOOTSTRAP= 1;
+        };
+      };
+    });
+  };
 }
+
